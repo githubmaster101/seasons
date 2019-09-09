bf335918dc263ea70a988a1ff2625e5759608a20
Kevin <yardman10550@gmail.com>
2019-09-09 16:50:52 -0400

    intial app layout

---
+++ src/SeasonsDisplay.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SeasonsDisplay = () => {
+    return (
+        <div style={{ textAlign: "center" }} >
+            <h1>Seasons Display Component</h1>
+            <h3>Spring</h3>
+            <h3>Summer</h3>
+            <h3>Autumn</h3>
+            <h3>Winter</h3>
+        </div>
+
+    );
+};
+
+export default SeasonsDisplay;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { loadPartialConfig } from '@babel/core';
+//import SeasonsDisplay from './SeasonsDisplay';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+    constructor(props) {
+        super(props);
+        // Initialize State 
+        this.state = { lat: null, errorMessage: '' };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        window.navigator.geolocation.getCurrentPosition((position) => {
+            // Update State 
+            this.setState({ lat: position.coords.latitude });
+        }, (err) => {
+            this.setState({ errorMessage: err.message });
+        });
+    }
+    // React requires us to define the render method
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error:{this.state.errorMessage} </div>
+        };
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+        return (<h3>loading</h3>);
+    };
+
+}
+
+ReactDOM.render(
+    <App />, document.querySelector('#userInterface')
+);
